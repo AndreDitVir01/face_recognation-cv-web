e17d8af22a743d0d77b599552f5eefdf97ea218e
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-04-18 08:22:29 +0700

    UPD:tugas_kel3_04.py

--- tugas_kel3_04.py
+++ tugas_kel3_04.py
@@ -2,6 +2,7 @@
 # import matplotlib.pyplot as plt
 # import cv2
 import os
+import array
 from skimage.feature import local_binary_pattern
 
 import numpy as np
@@ -49,7 +50,7 @@ for i in range(number_files):
 	nrp = '21031810' + str(j)
 
 	## Cetak nama file gambar lengkap beserta pathnya "untuk menunjukkan telah terbaca"
-	# print(maps_path + "\\" + image_name + "::" + str(nrp))
+	print(maps_path + "\\" + image_name + "::" + str(nrp))
 	## Increment nilai J
 	j+=1
 
@@ -82,13 +83,18 @@ for k in range(number_files):
 		### Pemberian kotak hijau untuk penanda simpan
 		cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		roi_color = img[y:y + h, x:x + w]
-		roi_gray = gray[y:y+h, x:x+w]
+		roi_gray = gray[y:y+h, x:x+w]	
+		### NRP 41 dan 58, 59 dilompati karena tidak ada mahasiswa dengan nrp tersebut
+		if(c==41):
+			c+=1
+		elif(c==58):
+			c+=2
 		# print("[INFO] Object found. Saving locally.")
 		cv2.imwrite('D:\\Teknik Informatika\\CV\\citra02\\citra02\\Dataset_Haar\\21031810' + str(c) +'_face.jpg', roi_gray)
 		c+=1
 
 ########## 3) LAKUKAN EKSTRAKSI FITUR FILE YANG DI DATASET_HAAR DAN SIMPAN DI DATABASE FITUR
-
+j = 31
 # Dataset_Haar	
 maps_path = 'D:\\Teknik Informatika\\CV\\citra02\\citra02\\Dataset_Haar'
 listds = os.listdir(maps_path)
@@ -153,7 +159,7 @@ for i in range(number_files):
 
 	nrp = '21031810' + str(j)
 	## Cetak nama file gambar lengkap beserta pathnya "untuk menunjukkan telah terbaca"
-	# print(maps_path + "\\" + image_name + "::" + str(nrp))
+	print(maps_path + "\\" + image_name + "::" + str(nrp))
 	## Increment nilai J
 	j+=1
 
@@ -177,7 +183,12 @@ for k in range(number_files):
 		### Pemberian kotak hijau untuk penanda simpan
 		cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		roi_color = img[y:y + h, x:x + w]
-		roi_gray = gray[y:y+h, x:x+w]
+		roi_gray = gray[y:y+h, x:x+w]	
+		### NRP 41 dan 58, 59 dilompati karena tidak ada mahasiswa dengan nrp tersebut
+		if(c==41):
+			c+=1
+		elif(c==58):
+			c+=2
 		# print("[INFO] Object found. Saving locally.")
 		cv2.imwrite('D:\\Teknik Informatika\\CV\\citra02\\citra02\\Testing_Haar\\21031810' + str(c) +'_face.jpg', roi_gray)
 		c+=1
@@ -191,7 +202,7 @@ number_files = len(listds)
 
 os.chdir(maps_path)
 arr = os.listdir()
-
+j = 31
 for i in range(number_files):
 	image_name = arr[i]
 	image = cv2.imread(image_name, 0)
@@ -225,78 +236,48 @@ for i in range(number_files):
 
 # fitur dataset dari dataset_haar
 for key,value in dataset.items():
+	new_value = array.array('f', [])
 	for o in range(6):
-		new_value = (value[o]/max(value))*100
-		dataset.update({key : new_value})
+		new_value.append((value[o]/max(value))*100)
+	dataset.update({key : new_value})
 
-# fitur tetsing dari testing_haar
-for key,value in testing.items():
+# fitur testing dari testing_haar
+for key2,value2 in testing.items():
+	new_value2 = array.array('f', [])
 	for o in range(6):
-		new_value = (value[o]/max(value))*100
-		testing.update({key : new_value})
+		new_value2.append((value2[o]/max(value2))*100)
+	testing.update({key2 : new_value2})
 
 ########## 8) FITUR WAJAH TESTING DIBANDINGKAN DENGAN DATABASE FITUR DAN CARI NILAI JARAK YANG PALING KECIL SEBAGAI JAWABANNYA
 
 # perbandingan nilai fitur
 
-
-selisih_perMahasiswa = []
-total_perMahasiswa = []
-best_name = None
+counterOfStatus=0
 for key,value in testing.items():
 	z=0
+	best_name = None
 	y=0
 	temp_score = 700
 	for key2,value2 in dataset.items():
+		selisih_perMahasiswa = array.array('f', [])
+		total_perMahasiswa=0
 		for o in range(6):
-			selisih_perMahasiswa[o] = value[o]-value2[o]
+			selisih_perMahasiswa.append(abs(value[o]-value2[o]))
 		for p in range(6):
-			total_perMahasiswa[z] += selisih_perMahasiswa[p]
-		if(total_perMahasiswa[z]<temp_score):
-			temp_score = total_perMahasiswa[z]
-			best_name = key2[y]
+			total_perMahasiswa+=selisih_perMahasiswa[p]
+		if(total_perMahasiswa<temp_score):
+			temp_score = total_perMahasiswa
+			best_name = key2
 		y+=1
 		z+=1
-	print(best_name)
+	status = "0"
+	if(key == best_name):
+		status = "1"
+		counterOfStatus+=1
+	print(key + '::best answer = ' + best_name + ' ::status = ' + status)
 
-# def match(counts):
-# 	best_score = 10
-# 	best_name = None
-# 	for key,value in dataset.items():
-# 		score = 0
-# 		for o in range(6):
-# 			score+=abs(value[o]-counts[o])
-# 		if score < best_score:
-# 			best_score = score
-# 			best_name = key
-# 	return best_name
-    
-# counterOfStatus = 0
-# for i in range(number_files):
-# 	image_name = arr[i]
-# 	image = cv2.imread(image_name, 0)
-# 	## Ekstraksi foto menjadi LBP
-# 	lbp = local_binary_pattern(image, n_points, radius, METHOD)
-# 	n_bins = int(lbp.max() + 1)
+# CETAK NAMA TERBAIK
 
-# 	hist, _ = np.histogram(lbp, density=True, bins=n_bins, range=(0,n_bins))
-
-# 	### NRP 41 dan 58, 59 dilompati karena tidak ada mahasiswa dengan nrp tersebut
-# 	if(j==41):
-# 		j+=1
-# 	elif(j==58):
-# 		j+=2
-
-# 	nrp = '21031810' + str(j)
-
-# 	status = "0"
-# 	if(nrp == match(hist)):
-# 		status = "1"
-# 		counterOfStatus+=1
-
-# 	print(nrp + ' ::best answer = ' + match(hist) + ' ' + status)
-# 	j+=1
-
-# print('Hasil yang cocok = '+ str(counterOfStatus))
-# print('Hasil yang tidak cocok = '+ str(27 - counterOfStatus))
-# print('Akurasi = ' + str((counterOfStatus/number_files)*100) + '%')
+print('Hasil yang cocok = '+ str(counterOfStatus))
+print('Hasil yang tidak cocok = '+ str(27 - counterOfStatus))
+print('Akurasi = ' + str((counterOfStatus/number_files)*100) + '%')
